d519a85e0fb0d4d5f59ff67dd731211e37a15308
Michael Lamb <lambm07@gmail.com>
2016-01-23 21:54:51 -0600

    command line arguments added

--- PA1/server.cpp
+++ PA1/server.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
-#include <sys/types.h>
+#include <netdb.h>
 #include <sys/socket.h>
+#include <string.h>
+#include <unistd.h>
+
+using namespace std;
 
 int getaddrinfo(const char *node,
 				const char *service,
 				const struct addrinfo *hints,
 				struct addrinfo **results);
 
-int main()
+int main(int argc, char* argv[])
 {
+	if(argc != 3)
+	{
+		cerr << "Usage: " << argv[0] << " requires DOMAIN and PORT arguments" << endl ;
+		return 1;
+	}
+	int status;
+	struct addrinfo host_info;
+	struct addrinfo *host_info_list;
+
+	memset(&host_info, 0, sizeof(host_info));
+
+
+
 	return 0;
 }
