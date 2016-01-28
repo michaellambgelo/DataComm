ef8a234f7aa4853ae4cb11b0bf9b0af63419e1fe
Michael Lamb <lambm07@gmail.com>
2016-01-28 13:33:50 -0600

    finished shit

--- PA1/client.cpp
+++ PA1/client.cpp
@@ -1,35 +1,119 @@
+//includes for general use
 #include <iostream>
-#include <netdb.h>
-#include <sys/socket.h>
-#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
+//includes for networking
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
 
 using namespace std;
 
-int getaddrinfo(const char *node, 		//domain or IP
-				const char *service, 	//"http" or port number
-				const struct addrinfo *hints,
-				struct addrinfo **results);
-
-int main(int argc, char* argv[])
+void error(const char *msg)
 {
-	if(argc != 4)
-	{
-		cerr << "Usage: " << argv[0] << " requires DOMAIN, PORT, FILENAME arguments" << endl ;
-		return 1;
-	}
+    perror(msg);
+    exit(0);
+}
 
-	int status;
-	struct addrinfo host_info;
-	struct addrinfo *host_info_list;
+int main(int argc, char *argv[])
+{
+    int sockfd, n_port, r_port, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
 
-	memset(&host_info, 0, sizeof(host_info)); //clear memory
-	hints.ai_family =  AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
+    FILE *filep;
 
-	host_info = argv[1];
-	int n_port = argv[2];
+    char buffer[4];
 
-	return 0;
+    if (argc < 4) //check command line args, need hostname, n_port, filename
+    {
+       fprintf(stderr,"usage %s hostname, negotiation port, filename\n", argv[0]);
+       exit(0);
+    }
+
+    //begin negotation stage----------------------------------------------------
+    n_port = atoi(argv[2]); 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("Error opening socket\n");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) 
+    {
+        fprintf(stderr,"Error, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr)); //clear variables
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(n_port); //negotiation port
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("Error connecting\n");
+
+    //request r_port by sending shared secret '117'
+    char secret[] = "117";
+    n = write(sockfd,secret,sizeof(secret));
+    if(n < 0)
+        error("Unable to establish trust with server");
+
+    n = read(sockfd,&r_port,sizeof(r_port)); //get the r_port
+    if(n < 0)
+        error("Error getting transfer port\n");
+    
+    printf("Communicating with server. Transferring file on port %d\n",ntohs(r_port));
+   
+    close(sockfd);
+
+    //begin UDP transfer stage---------------------------------------------------
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0); //SOCK_DGRRAM for UDP
+    if (sockfd < 0) 
+        error("Error opening socket\n");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) 
+    {
+        fprintf(stderr,"Error, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr)); //clear variables
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(r_port); //negotiation port
+    //if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+    //    error("Error: unable to connect()\n");
+
+/*    n = sendto(sockfd, secret, sizeof(secret), 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    if (n < 0)
+        error("Error sending UDP message");
+    printf("Secret sent\n");
+    fflush(stdout);*/
+
+/*    n = recvfrom(sockfd, buffer, strlen(buffer), 0, NULL, NULL);
+    if(n < 0)
+        error("Error receiving ack");
+*/
+    filep = fopen(argv[3],"r");
+    if(filep == NULL)
+        error("Error opening file\n");
+
+    bzero(buffer, strlen(buffer));
+    while(fgets(buffer,4,filep))
+    {
+        size_t length = strlen(buffer);
+        n = sendto(sockfd, buffer, length, 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+        if(n < 0)
+            error("Error sending payload\n");
+        n = recvfrom(sockfd, buffer, length, 0, NULL, NULL);
+        if(n < 0)
+            error("Error receiving payload\n");
+        printf("%s\n",buffer);
+    }
+    char eof[] = "\0";
+    n = sendto(sockfd, eof, sizeof(eof), 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    fclose(filep);
+    printf("File transfer complete\n");
+
+    return 0;
 }
--- PA1/server.cpp
+++ PA1/server.cpp
@@ -1,47 +1,153 @@
+//includes for general use
 #include <iostream>
-#include <netdb.h>
-#include <sys/socket.h>
-#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
 #include <unistd.h>
 #include <time.h>
+#include <ctype.h> //for toupper()
+
+//includes for networking
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 using namespace std;
 
-int main(int argc, char* argv[])
+int randomPort(int n_port) //pick a random port
 {
-	if(argc != 3)
-	{
-		cerr << "Usage: " << argv[0] << " requires DOMAIN and PORT arguments" << endl ;
-		return 1;
-	}
+    int val = n_port;
+    srand(time(NULL));
+    while(val == n_port)
+        val = rand() % 65535 + 1024;
 
-	int r_port = srand(time(NULL)) % 65535 + 1023;
+    return val;
+}
 
-	int status;
-	struct addrinfo host_info;
-	struct addrinfo *servinfo;
-	struct sockaddr_storage connect_ip; // connector's address information
+void error(const char *msg) //display messages on sys call errors
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, newsockfd, tcpsockfd, n_port, r_port;
+    socklen_t clilen;
+    char buffer[4];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n, o;
+    FILE *filep;
+
+    if (argc < 2) //check command line args, need n_port
+    {
+        fprintf(stderr,"ERROR, no negotiation port provided\n");
+        exit(1);
+    }
+    
+    //begin negotation stage----------------------------------------------------
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    bzero((char *) &serv_addr, sizeof(serv_addr)); //clear variables
+
+    n_port = atoi(argv[1]); //convert char*[] arg to int
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(n_port); //host to network short
+
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+        error("Error: unable to bind()");
+
+    // listen() and accept()
+    listen(sockfd,5); //listen with a queue of 5
+    clilen = sizeof(cli_addr);
+    //new file descriptor for connection to client
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if (newsockfd < 0) 
+        error("Error: unable to accept()");
+    
+    //wait for client to request r_port
+    n = read(newsockfd,buffer,sizeof(buffer)); 
+    if(n < 0)
+        error("Error receiving message from client");
+    if(strcmp(buffer, "117") != 0)
+    {
+        printf("Invalid request ID: %s",buffer);
+        exit(4);
+    }
+
+    r_port = randomPort(n_port); 
+    //send r_port
+    printf("Negotiation detected. Selected random port %d\n",ntohs(r_port));
+    n = write(newsockfd,&r_port,sizeof(r_port));
+
+    close(newsockfd);
+    close(sockfd);
+    
+    //begin UDP transfer stage--------------------------------------------------
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) 
+        error("Error: unable to open socket");
+    bzero((char *) &serv_addr, sizeof(serv_addr)); //clear variables
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(r_port); //host to network short
+
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+        error("Error: unable to bind()");
+
+/*    // listen() and accept()
+    listen(sockfd,5); //listen with a queue of 5
+    clilen = sizeof(cli_addr);
+    //new file descriptor for connection to client
+    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+    if (newsockfd < 0) 
+        error("Error: unable to accept()\n");
+
+    printf("Accepted file transfer on port %d\n",r_port);*/
+
+    size_t length = strlen(buffer);
+
+    filep = fopen("received.txt","a");
+/*    do
+    {
+        n = recvfrom(sockfd, buffer, 4, 0, NULL, NULL);
+        if (n < 0)
+            error("Error receiving");
+
+    }while(strcmp(buffer,"117") != 0);
+    printf("%s receieved. Accepting file transfer.\n",buffer);
+    fflush(stdout);
+    bzero(buffer, length);
+*/
+    n = 1;
+    while(n > 0)
+    {
+        n = recvfrom(sockfd, buffer, 4, 0, (struct sockaddr*)&cli_addr, &clilen);
+        if(n < 0)
+            error("Error receiving message\n");
+        fprintf(filep,buffer);
+        length = strlen(buffer);
+
+        if(strcmp(buffer,"\0") == 0)
+            n = 0;
 
 
-	memset(&host_info, 0, sizeof(host_info)); //clear memory
-	hints.ai_family =  AF_UNSPEC; //unspecified, IPv4/IPv6
-	hints.ai_socktype = SOCK_STREAM; //TCP
-	hints.ai_flags = AI_PASSIVE;
-
-	host_info = argv[1]; //domain or IP
-	int n_port = argv[2]; //negotiation port
-
-	if ((status = getaddrinfo(NULL, n_port, &hints, &servinfo)) != 0) 
-	{
-	    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-	    exit(1);
-	}
-
-	// make a socket:
-	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-
-	// bind it to the port we passed in to getaddrinfo():
-	bind(sockfd, res->ai_addr, res->ai_addrlen);
-
-	return 0;
+        for(int i = 0; i < length; i++)
+        {
+            buffer[i] = toupper(buffer[i]);
+        }
+        if(buffer[length - 1] == '\0')
+            continue;
+        o = sendto(sockfd, buffer, length, 0, (struct sockaddr*)&cli_addr, clilen);
+        if(o < 0)
+            error("Error sending ack\n");
+        bzero(buffer,length);
+    }
+    
+    fclose(filep);
+    close(sockfd);
+    return 0; 
 }
